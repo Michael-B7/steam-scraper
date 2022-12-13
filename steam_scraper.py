ff3790a4586b4b4a5a13dbf0310b9618b2380e7d
Bairs7 <mbrashears2006@gmail.com>
2022-12-13 13:24:14 -0800

    functionalized

--- steam_scraper.py
+++ steam_scraper.py
@@ -2,24 +2,42 @@ from bs4 import BeautifulSoup
 import requests
 
 URL = 'https://steamcharts.com/'
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-
-top = soup.find(id = 'top-games')
-
-games = top.find_all(class_ = 'game-name')
-
-names = []
-
-for i in range(0, len(games)):
-    names.append(games[i].find('a'))
-
-namesC = names[:]
-length = len(namesC)
-
-def find_clean(names):
+def get_top():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
     
+    top = soup.find(id = 'top-games')
+    games = top.find_all(class_ = 'game-name')
+    
+    names = []
+    
+    for i in range(0, len(games)):
+        names.append(games[i].find('a'))
+    
+    return names
+
+def get_data(game):
+
+    url = f"{URL}search/?q= {game}"
+
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    games = soup.find_all('td')
+    
+    names = []
+
+    for i in range(0, len(games)):
+        names.append(games[i].find('a'))
+
+    return names
+
+def clean(names):
+
+    for i in range(0,len(names)):
+        names[i] = str(names[i])
+
     removes = []
     
     for a in range(0,len(names)):
@@ -34,15 +52,7 @@ def find_clean(names):
                 removes.append(remove)
                 remove = ''
     
-    return removes
-
-def clean(names):
-    
     namesTemp = []
-    
-    removes = find_clean(names)
-    
-    print(removes)
     
     for a in range(0, len(names)):
         
@@ -69,3 +79,5 @@ def pair(names, players):
         pairs.update({names[i]:players[i]})
     
     return pairs
+
+print(clean(get_top()))
