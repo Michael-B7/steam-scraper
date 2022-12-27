8fbb12825892e9c39c6a3e34b6aee8fcd29b3560
Michael B <mbrashears2006@gmail.com>
2022-12-26 16:49:47 -0800

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <html>
     <head>
         <title>Steam Scraper</title>
-        
     </head>
     <body>
+        <p>hello world</p>
         <p>
             <?PHP
             echo shell_exec("python steam_scraper.py 'parameter1'");
